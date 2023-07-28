fd65849d506e4ae4efbc505db374e5a91d16fb81
joeinman <joe.inman8@gmail.com>
2023-07-28 11:53:39 +0100

    Fixed Algorithms.

--- src/CRC32.cpp
+++ src/CRC32.cpp
@@ -2,57 +2,53 @@
 
 namespace CRC32
 {
-    constexpr uint32_t CRC32_POLYNOMIAL  = 0x04C11DB7;
-    constexpr uint32_t CRC32C_POLYNOMIAL = 0x1EDC6F41;
-    constexpr uint32_t BIT_31_MASK       = 0x80000000;
-    constexpr int      BYTE_TO_BIT_SHIFT = 24;
 
     uint32_t crc32(const uint8_t *data, size_t size)
     {
-        uint32_t remainderPolynomial = 0;
+        uint32_t generatorPolynomial = 0x04C11DB7;
+        uint32_t remainderPolynomial = 0xFFFFFFFF;
 
         for (size_t i = 0; i < size; i++)
         {
-            remainderPolynomial ^= (data[i] << BYTE_TO_BIT_SHIFT);
-
+            remainderPolynomial ^= static_cast<uint32_t>(data[i]) << 24;
             for (int j = 0; j < 8; j++)
             {
-                if (remainderPolynomial & BIT_31_MASK)
+                if (remainderPolynomial & 0x80000000)
                 {
-                    remainderPolynomial = (remainderPolynomial << 1) ^ CRC32_POLYNOMIAL;
+                    remainderPolynomial = (remainderPolynomial << 1) ^ generatorPolynomial;
                 }
                 else
                 {
-                    remainderPolynomial = (remainderPolynomial << 1);
+                    remainderPolynomial <<= 1;
                 }
             }
         }
 
-        return remainderPolynomial;
+        return remainderPolynomial ^ 0xFFFFFFFF;
     }
 
     uint32_t crc32c(const uint8_t *data, size_t size)
     {
-        uint32_t remainderPolynomial = 0;
+        uint32_t generatorPolynomial = 0x1EDC6F41;
+        uint32_t remainderPolynomial = 0xFFFFFFFF;
 
         for (size_t i = 0; i < size; i++)
         {
-            remainderPolynomial ^= (data[i] << BYTE_TO_BIT_SHIFT);
-
+            remainderPolynomial ^= static_cast<uint32_t>(data[i]) << 24;
             for (int j = 0; j < 8; j++)
             {
-                if (remainderPolynomial & BIT_31_MASK)
+                if (remainderPolynomial & 0x80000000)
                 {
-                    remainderPolynomial = (remainderPolynomial << 1) ^ CRC32C_POLYNOMIAL;
+                    remainderPolynomial = (remainderPolynomial << 1) ^ generatorPolynomial;
                 }
                 else
                 {
-                    remainderPolynomial = (remainderPolynomial << 1);
+                    remainderPolynomial <<= 1;
                 }
             }
         }
 
-        return remainderPolynomial;
+        return remainderPolynomial ^ 0xFFFFFFFF;
     }
 
 } // namespace CRC32
