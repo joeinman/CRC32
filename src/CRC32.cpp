a97d3ac0bd78c9c24c15a38b4dbd7c1af5517db6
joeinman <joe.inman8@gmail.com>
2023-07-28 20:03:30 +0100

    Removed Duplicate Code.

--- src/CRC32.cpp
+++ src/CRC32.cpp
@@ -2,10 +2,8 @@
 
 namespace CRC32
 {
-
-    uint32_t crc32(const uint8_t *data, size_t size)
+    uint32_t calculateGeneric(const uint8_t *data, size_t size, uint32_t generatorPolynomial)
     {
-        uint32_t generatorPolynomial = 0xEDB88320; // This is the reversed polynomial 0x04C11DB7
         uint32_t remainderPolynomial = 0xFFFFFFFF;
 
         for (size_t i = 0; i < size; i++)
@@ -27,28 +25,14 @@ namespace CRC32
         return remainderPolynomial ^ 0xFFFFFFFF;
     }
 
+    uint32_t crc32(const uint8_t *data, size_t size)
+    {
+        return calculateGeneric(data, size, 0xEDB88320); // This is the reversed polynomial 0x04C11DB7
+    }
+
     uint32_t crc32c(const uint8_t *data, size_t size)
     {
-        uint32_t generatorPolynomial = 0x82F63B78; // This is the reversed polynomial 0x1EDC6F41
-        uint32_t remainderPolynomial = 0xFFFFFFFF;
-
-        for (size_t i = 0; i < size; i++)
-        {
-            remainderPolynomial ^= data[i];
-            for (int j = 0; j < 8; j++)
-            {
-                if (remainderPolynomial & 1)
-                {
-                    remainderPolynomial = (remainderPolynomial >> 1) ^ generatorPolynomial;
-                }
-                else
-                {
-                    remainderPolynomial >>= 1;
-                }
-            }
-        }
-
-        return remainderPolynomial ^ 0xFFFFFFFF;
+        return calculateGeneric(data, size, 0x82F63B78); // This is the reversed polynomial 0x1EDC6F41
     }
 
 } // namespace CRC32
